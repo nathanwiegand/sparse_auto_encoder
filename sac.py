8f990574200861801bbf7a20c2ac91b95539e9bf
Nathan Wiegand <nathanwiegand@gmail.com>
2011-05-06 18:09:05 -0700

    Added lots of comments.  Literate programming and all that.

--- sac.py
+++ sac.py
@@ -3,17 +3,18 @@ import matplotlib.pyplot as plt
 import scipy.optimize
 import utils
 
+# This method is used to enforce the dimensionality of matrices since NumPy is a
+# bit aggressive about allowing operators over non-matching dimensions.
 def ASSERT_SIZE(matrix, shape):
   if matrix.shape != shape:
     raise AssertionError("Wrong shape: %s expexted: %s" %
                             (matrix.shape, shape))
 
+# This wraps the parameters for the sparse autoencoder.
 class SparseAutoEncoderOptions:
-  ##############################################################################
-  #  These network parameters are specified by by Andrew Ng specifically for   #
-  #  the MNIST data set here:                                                  #
-  #     http://ufldl.stanford.edu/wiki/index.php/Exercise:Vectorization        #
-  ##############################################################################
+  #  These network parameters are specified by by Andrew Ng specifically for
+  #  the MNIST data set here:
+  #     [[http://ufldl.stanford.edu/wiki/index.php/Exercise:Vectorization]]
   def __init__(self,
                visible_size,
                hidden_size,
@@ -28,6 +29,9 @@ class SparseAutoEncoderOptions:
     self.beta = beta
     self.output_dir = output_dir
 
+# The SparseAutoEncoder object wraps all the data needed in order to train a
+# sparse autoencoder.  Its constructor takes a SparseAutoEncoderOptions and a
+# v x m matrix where v is the size of the visible layer of the network.
 class SparseAutoEncoder:
   def __init__(self, options, data):
     self.options = options
@@ -35,10 +39,12 @@ class SparseAutoEncoder:
 
     self.frame_number = 0
 
+  # Convert the matrices to a flat vector.  This is needed by 'fmin_l_bfgs_b'.
   def flatten(self, W1, W2, b1, b2):
     return np.array(np.hstack([W1.ravel('F'), W2.ravel('F'),
                                b1.ravel('F'), b2.ravel('F')]), order='F')
 
+  # Convert the flat vector back to the W1, W2, b1, and b2 matrices.
   def unflatten(self, theta):
     hidden_size = self.options.hidden_size
     visible_size = self.options.visible_size
@@ -49,6 +55,7 @@ class SparseAutoEncoder:
     b2 = theta[2*hv+hidden_size:].reshape([visible_size, 1], order='F')
     return (W1, W2, b1, b2)
 
+  # Create the random values for the parameters to begin learning.
   def initialize_parameters(self):
     hidden_size = self.options.hidden_size
     visible_size = self.options.visible_size
@@ -60,9 +67,12 @@ class SparseAutoEncoder:
 
     return self.flatten(W1, W2, b1, b2)
 
+  # <div class='math'>1/(1 + e^{-x})</div>
   def sigmoid(self, x):
     return 1.0 / (1.0 + np.exp(-x))
 
+  # Compute the cost function J and the gradient for an input.  Note that this
+  # takes a flattened W1, W2, b1, b2 because of fmin_l_bfgs_b.
   def sparse_autoencoder(self, theta):
     visible_size = self.options.visible_size
     hidden_size = self.options.hidden_size
@@ -85,9 +95,10 @@ class SparseAutoEncoder:
                                                        self.frame_number))
     self.frame_number += 1
 
-    # Forward pass
-    z2 = np.dot(W1, x) + b1 # Note: even though the dimensionality doesn't
-                            # match, numpy will apply b1 to every column.
+    # ==Forward pass==
+    # Note: even though the dimensionality doesn't match because <p>$$b1$$</p>
+    # is a vector, numpy will apply b1 to every column.
+    z2 = np.dot(W1, x) + b1
     a2 = self.sigmoid(z2)
     ASSERT_SIZE(a2, (hidden_size, m))
 
@@ -104,7 +115,9 @@ class SparseAutoEncoder:
            (lamb/2.)*(np.sum(W1**2) + np.sum(W2**2)) + \
            beta*np.sum(kl)
 
-    y = x # we're learning an identity function
+    # We set <span class='math'>y</span> equal to the input since we're learning
+    # an identity function
+    y = x
     delta3 = -(y - a3) * a3*(1-a3)
     ASSERT_SIZE(delta3, (visible_size, m))
 
@@ -117,9 +130,10 @@ class SparseAutoEncoder:
     W2_grad = 1./m * np.dot(delta3, a2.T) + lamb * W2
     ASSERT_SIZE(W2_grad, (visible_size, hidden_size))
 
-    b2_grad = 1./m * np.sum(delta3, 1)[:, np.newaxis] # [:, newaxis] makes
-                                                      # this into a matrix
+    # [:, newaxis] makes this into a matrix
+    b2_grad = 1./m * np.sum(delta3, 1)[:, np.newaxis]
     ASSERT_SIZE(b2_grad, (visible_size, 1))
+
     # sum the rows of delta3 and then mult by  1/m
     W1_grad = 1./m * np.dot(delta2, x.T) + lamb * W1
     ASSERT_SIZE(W1_grad, (hidden_size, visible_size))
@@ -130,6 +144,8 @@ class SparseAutoEncoder:
     grad = self.flatten(W1_grad, W2_grad, b1_grad, b2_grad)
     return (cost, grad)
 
+  # Actually run gradient descent.  Call mySparseAutoEncoder.learn() to learn
+  # the parameters of W1, W2, b1, and b2 for this network and this data.
   def learn(self):
     def f(theta):
       return self.sparse_autoencoder(theta)
